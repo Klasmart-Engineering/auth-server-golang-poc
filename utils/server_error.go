33774225cebb467e8a2e3a5b202d3e165886c5c4
Matt Revell <nightowlmatt@gmail.com>
2022-04-29 08:18:38 +0100

    Fix ServerErrorResponse to output error to log

--- utils/server_error.go
+++ utils/server_error.go
@@ -1,19 +1,13 @@
 package utils
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 // ServerErrorResponse - This is a helper function to send a response when an error occurs
-// TODO: This currently sends actual error output to the client. It should probably only print to logs instead
-func ServerErrorResponse(statusCode int, w http.ResponseWriter, e error) {
+func ServerErrorResponse(statusCode int, w http.ResponseWriter, err error) {
 	w.WriteHeader(statusCode)
-	_, err := fmt.Fprintf(w, e.Error())
-	if err != nil {
-		log.Printf("Failed to send error response: %v", err.Error())
-		log.Printf("Original error: %v", e.Error())
-	}
+	log.Printf("Error: %e", err)
 	return
 }
